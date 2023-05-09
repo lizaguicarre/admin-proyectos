17a65193472fe2a6f55b4ba5df436b794c187740
lizaguicarre <A01382995@tec.mx>
2023-05-09 16:47:43 -0600

    ann

--- proyecto.py
+++ proyecto.py
@@ -4,25 +4,27 @@ warnings.filterwarnings(action='ignore', category=ConvergenceWarning)
 
 # Librerías: pandas, sklearn, tensorflow
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn import metrics
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
 
 # Carga de datos
-data = pd.read_excel('/Users/liz/Downloads/SEXTO SEMESTRE/CONCENTRACIÓN - INTELIGENCIA ARTIFICIAL CON IMPACTO EMPRESARIAL/RETO/DATOS RETO/ventas 2022.xlsx')
+data = pd.read_excel('C:/Users/usuario/Downloads/Tamborrel Data (4)/Tamborrel Data/Casas Vendidas The Woodlands/ventas 2022.xlsx')
 
 # División entre test (30%) y train (70%)
 train_data = data.sample(frac=0.7, random_state=42)
 test_data = data.drop(train_data.index)
 ## Divisiòn para redes neuronales
-x_columns = data.columns.drop('close_price')
-x = data[x_columns].values
-y = data['close_price'].values
-x_train, x_test, y_train, y_test = train_test_split(    
-    x, y, test_size=0.30, random_state=42)
+#x_columns = data.columns.drop('close_price')
+#x = data[x_columns].values
+#y = data['close_price'].values
+#x_train, x_test, y_train, y_test = train_test_split(    
+#    x, y, test_size=0.30, random_state=42)
 
 # Variables independientes
 input_cols = ['building_sqft', 'bedrooms', 'baths_total']
@@ -39,7 +41,7 @@ test_outputs = test_data[output_cols]
 # Redes Neuronales
 # Especificación del modelo
 nn_model = Sequential()
-nn_model.add(Dense(25, input_dim=x.shape[1], activation='relu')) # Hidden 1
+nn_model.add(Dense(25, input_dim=3, activation='relu')) # Hidden 1
 nn_model.add(Dense(10, activation='relu')) # Hidden 2
 nn_model.add(Dense(1)) # Output
 nn_model.compile(loss='mean_squared_error', optimizer='adam')
@@ -48,14 +50,13 @@ monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3,
                         restore_best_weights=True)
 
 # Prueba del modelo
-nn_model.fit(x_train,y_train,validation_data=(x_test,y_test),
-          callbacks=[monitor],verbose=2,epochs=1000)
+nn_model.fit(train_inputs, train_outputs, epochs=50, batch_size=32, validation_split=0.2, callbacks=[monitor])
 
 # Evaluación del modelo
-nn_score = nn_model.score(test_inputs, test_outputs)
+nn_preds = nn_model.predict(test_inputs)
+nn_mse = metrics.mean_squared_error(test_outputs, nn_preds)
+nn_score = np.sqrt(nn_mse) #RMSE
 
-# Predecir con el modelo
-nn_price = nn_model.predict(input_data)
 #nn_rmse = metrics.mean_squared_error(pred,y_test)
 # ------------------------------------------------------------------------------------------------------------
 # Modelo 2
@@ -76,8 +77,12 @@ grid_search.fit(train_inputs, train_outputs)
 best_rf_model = grid_search.best_estimator_
 best_rf_model.fit(train_inputs, train_outputs)
 
+# Predicciones iniciales con el modelo
+y_pred = best_rf_model.predict(test_inputs)
+
 # Evaluación del modelo
-rf_score = best_rf_model.score(test_inputs, test_outputs)
+rf_score = rmse = np.sqrt(metrics.mean_squared_error(test_outputs, y_pred))
+
 # -----------------------------------------------------------------------------------------------------------
 
 # Predicciones
@@ -90,7 +95,8 @@ num_bathrooms = float(input("Número de baños completos: "))
 
 input_data = [[num_rooms, sqft, num_bathrooms]]
 
-# Modelo 1 - Predicciones
+# Neural Network - Predicciones
+nn_price = nn_model.predict(input_data)
 # Modelo 2 - Predicciones
 # Modelo 3 - Predicciones
 # Random Forest - Predicciones
@@ -98,7 +104,7 @@ rf_price = best_rf_model.predict(input_data)
 
 print("------------------------------------------------------------------------------------------------------")
 print("Resultados:")
-print("Precio estimado con redes neuronales: ${:.2f}, con precisión del: {:.2f}%".format(nn_price[0], nn_score * 100))
-# print("Precio estimado con modelo 2: ${:.2f}, con precisión del: {:.2f}%".format(modelo2_price[0], modelo2_score * 100))
-# print("Precio estimado con modelo 3: ${:.2f}, con precisión del: {:.2f}%".format(modelo3_price[0], modelo3_score * 100))
-print("Precio estimado con random forest: ${:.2f}, con precisión del: {:.2f}%".format(rf_price[0], rf_score * 100))
+print("Precio estimado con redes neuronales: ${:.2f}, con un RMSE del: {:.2f}".format(float(nn_price), nn_score))
+# print("Precio estimado con modelo 2: ${:.2f}, con un RMSE del: {:.2f}%".format(modelo2_price[0], modelo2_score * 100))
+# print("Precio estimado con modelo 3: ${:.2f}, con un RMSE del: {:.2f}%".format(modelo3_price[0], modelo3_score * 100))
+print("Precio estimado con random forest: ${:.2f}, con un RMSE del: {:.2f}".format(rf_price[0], rf_score))
