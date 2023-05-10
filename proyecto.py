4237ea8cb15a0af1dc87db3ec59da6fe4855a007
marcoberlanga10 <marcoberlangamendoza@gmail.com>
2023-05-09 18:01:13 -0600

    Regresión Lineal Marco Berlanga
    Código

--- proyecto.py
+++ proyecto.py
@@ -29,6 +29,50 @@ test_outputs = test_data[output_cols]
 # Modelo 1
 # ------------------------------------------------------------------------------------------------------------
 # Modelo 2
+
+# Librerías: pandas, sklearn, tensorflow
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.exceptions import NotFittedError
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import statsmodels.api as sm
+
+
+# Carga de datos
+data = pd.read_excel('C:/Users/usuario/Downloads/Tamborrel Data (4)/Tamborrel Data/Casas Vendidas The Woodlands/ventas 2022.xlsx')
+
+# División entre test (30%) y train (70%)
+train_data = data.sample(frac=0.7, random_state=42)
+test_data = data.drop(train_data.index)
+
+# Variables independientes
+input_cols = ['building_sqft', 'bedrooms', 'baths_total']
+# Variable dependiente
+output_cols = ['close_price']
+
+train_inputs = train_data[input_cols]
+train_outputs = train_data[output_cols]
+
+test_inputs = test_data[input_cols]
+test_outputs = test_data[output_cols]
+
+# Añadir constante
+X = sm.add_constant(train_inputs)
+
+# Modelo de regresión
+model = sm.OLS(train_outputs, X).fit()
+
+
 # ------------------------------------------------------------------------------------------------------------
 # Modelo 3
 # ------------------------------------------------------------------------------------------------------------
@@ -63,6 +107,26 @@ input_data = [[num_rooms, sqft, num_bathrooms]]
 
 # Modelo 1 - Predicciones
 # Modelo 2 - Predicciones
+
+# Predicciones en test data
+X_test = sm.add_constant(test_inputs)
+lm_pred1 = model.predict(X_test)
+
+# Cálculo de RMSE
+lm_rmse = np.sqrt(mean_squared_error(test_outputs, lm_pred1))
+
+# Input del usuario
+sqft = float(input("Metros cuadrados: "))
+num_rooms = int(input("Número de cuartos: "))
+num_bathrooms = float(input("Número de baños completos: "))
+
+input_data = [[num_rooms, sqft, num_bathrooms]]
+
+input_data_augmented = np.insert(input_data, 0, 1, axis=1) # add a column of ones
+lm_price = model.predict(input_data_augmented)[0]
+
+print("Precio estimado con modelo 2: ${:.2f}, con un RMSE del: {:.2f}".format(lm_price, lm_rmse))
+
 # Modelo 3 - Predicciones
 # Random Forest - Predicciones
 rf_price = best_rf_model.predict(input_data)
